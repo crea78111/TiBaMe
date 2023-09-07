8576c3b8400ee9a2b5cc39f47b1852f19012edda
Claire K <crea78111@gmail.com>
2023-09-07 16:46:23 +0800

    add new file to github

---
+++ src/xxxx/HelloGitHub.java
@@ -0,0 +1,13 @@
+package xxxx;
+
+public class HelloGitHub {
+
+	public HelloGitHub() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
